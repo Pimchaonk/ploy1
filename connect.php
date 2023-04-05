46fbb13d60171ecaffedd45de0dd3bdab8df7ada
maayasuo <thean2000@gmail.com>
2023-04-05 23:45:15 +0700

    Azure db implementation

--- connect.php
+++ connect.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pos_system";
+    $host = 'pos-system.mysql.database.azure.com';
+    $username = 'bhodi';
+    $password = 'Treebangbang*007';
+    $db_name = 'pos_system';
 
-// Create connection
-$mysqli = new mysqli($servername, $username, $password, $dbname);
+    //Initializes MySQLi
+    $conn = mysqli_init();
 
-// Check connection
-if ($mysqli->connect_error) {
-  die("Connection failed: " . $mysqli->connect_error);
-}
+    mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+    // Establish the connection
+    mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin@mydemoserver', 'yourpassword', 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+    //If connection failed, show the error
+    if (mysqli_connect_errno())
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
 ?>
