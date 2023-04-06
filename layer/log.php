bbdb166efa469ad16f6c62224bb40b25b9e43a87
Bhodikorn <112610961+Bhodikorn@users.noreply.github.com>
2023-04-06 20:09:30 +0700

    Update log.php

--- layer/log.php
+++ layer/log.php
@@ -3,14 +3,17 @@
     // Set the location and name of your log file
     $log_file = '..\layer\myapp.log';
 
-    // Get the IP address and timestamp of the user
-    $ip_address = $_SERVER['REMOTE_ADDR'];
-    $timestamp = date('Y-m-d H:i:s');
-
-    // Format the log entry
-    $log_entry = "[$timestamp] User with IP address $ip_address logged in.\n";
-
-    // Write the log entry to the log file
-    error_log($log_entry, 3, $log_file);
+    function log_sqli($string)
+    {   
+    date_default_timezone_set('Asia/Bangkok');
+   
+    $ip = $_SERVER['REMOTE_ADDR'];    
+    $date = date('Y-m-d H:i:s');
     
+    // Write the log to file
+    $log = fopen('..\layer\myapp.log', 'a');
+
+    fwrite($log, "$date | IP: $ip | Message: $string |SQL Injection detected\n");
+    fclose($log);
+    }
     ?>
