6f44c6a048480d5fca233ae1536df112f5e1147c
Bhodikorn <112610961+Bhodikorn@users.noreply.github.com>
2023-04-23 15:41:29 +0700

    Update detect-sqli.php

--- layer/detect-sqli.php
+++ layer/detect-sqli.php
@@ -5,16 +5,14 @@ require_once('../layer/log-sqli.php');
 function detect_sqli($string)
 {
     // Set a score threshold to reduce false positives
-    $scoreThreshold = 2;
+    $scoreThreshold = 3;
     $score = 0;
 
     // Layer 1: Check for commonly used SQL injection characters
     $regex1 = array(
         "/'/",
         '/"/',
-        '/\/\*/',
-        "/=/",
-        '/^\s*(--|#|\/\*)/',
+        '/^\s*(--|#)/',
     );
 
     foreach ($regex1 as $pattern) {
@@ -25,9 +23,9 @@ function detect_sqli($string)
 
     // Layer 2: Check for common SQL injection keywords and operators
     $regex2 = array(
-        '/\b(select|union|insert|update|delete|drop|truncate|alter|exec|xp_cmdshell|sp_executesql|create|declare)\b/i',
+        '/\b(alter|select|union|insert|update|delete|drop|truncate|exec|create|declare)\b/i',
         '/\b(from|into|set|where|and|or|not|like|in|between|is)\b/i',
-        '/[\=\>\<\!\~]/',
+        '/[\=\>\<]/',
         '/[\%|\_]/',
     );
 
@@ -39,17 +37,41 @@ function detect_sqli($string)
 
     // Layer 3: Check for special SQL injection payloads and techniques
     $regex3 = array(
-        '/\s*(\ball\b|\bany\b|\bnot\b|\band\b|\bbetween\b|\bin\b|\blike\b|\bor\b|\bsome\b|\bcontains\b|\bcontainsall\b|\bcontainskey\b)\s*.+[\=\>\<=\!\~]+.+/i',
-        '/\s*(\blet\b|\bdeclare\b|\bbegin\b|\bend\b|\bif\b|\belse\b|\bwhile\b|\bfor\b|\bcase\b|\bswitch\b|\binto\b)\s*.+[\=\>\<=\!\~]+.+/i',
+        '/\s*(\ball\b|\bany\b|\bnot\b|\band\b|\bbetween\b|\bin\b|\blike\b|\bor\b|\bsome\b|\bcontains\b|\bcontainsall\b|\bcontainskey\b)\s*.+[\=\>\<]+.+/i',
+        '/\s*(\blet\b|\bdeclare\b|\bbegin\b|\bend\b|\bif\b|\belse\b|\bwhile\b|\bfor\b|\bcase\b|\bswitch\b|\binto\b|\bdelay\b)\s*.+[\=\>\<]+.+/i',
         '/\b(select|update).+?(from|set)/i',
-        '/\b(union\s+all\s+select|select\s+union\s+all)/i',
         '/\b(?:sleep|benchmark)\b\s*\(/i',
         '/[\)\(\;\,\|\&\-\+]/',
     );
 
     foreach ($regex3 as $pattern) {
         if (preg_match($pattern, $string)) {
-            $score += 2;
+            $score ++;
+        }
+    }
+
+    // Layer 4: Check for hex encoding and URL encoding
+    $regex4 = array(
+        '/(?:%[0-9a-f]{2})+/i',
+        '/0x[0-9a-f]+/i',
+    );
+
+    foreach ($regex4 as $pattern) {
+        if (preg_match($pattern, $string)) {
+            $score++;
+        }
+    }
+
+    // Layer 5: Check for escape characters and comment characters
+    $regex5 = array(
+        '/\\\\/',
+        '/#\+/',
+        '/;/',
+    );
+
+    foreach ($regex5 as $pattern) {
+        if (preg_match($pattern, $string)) {
+            $score++;
         }
     }
 
@@ -57,6 +79,7 @@ function detect_sqli($string)
         log_sqli($string);
         return true;
     } else {
+        love($string);
         return false;
     }
 }
