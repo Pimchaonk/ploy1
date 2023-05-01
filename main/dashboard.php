ff7b9bdf5fc296eb3d1ddf6dfd7133217956e8ec
maayasuo <thean2000@gmail.com>
2023-05-01 22:08:38 +0700

    rollback to original

--- main/dashboard.php
+++ main/dashboard.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-// if (!isset($_SESSION["user"])) {
-//   header("Location: ../authentication/login.php");
-// }
+if (!isset($_SESSION["user"])) {
+  header("Location: ../authentication/login.php");
+}
 
 ?>
 
